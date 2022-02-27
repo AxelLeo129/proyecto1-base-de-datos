7cff6c66c516772885a8c2bfdb1cfd77a22f048a
AxelLeo129 <axelleo129dev@gmail.com>
2022-02-26 23:19:02 -0600

    Last changes.

--- index.js
+++ index.js
@@ -8,41 +8,58 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("adsfadsf");
+  res.send("API llenado tabla Postgres with CSV.");
 });
 
 app.post("/upload-file", (req, res) => {
-  const { file, host, user, database, password, port, table, fields } = req.body;
-  let stream = fs.createReadStream(file);
-  let csvData = [];
-  let csvStream = fastcsv.parse().on("data", function (data) { csvData.push(data); }).on("end", function () {
-      csvData.shift();
-      const pool = new Pool({ host, user, database, password, port });
-      let values = '';
-      const values_string = fields.split(',');
-      for(let i = 0; i < values_string.length; i++) {
-          values += '$' + i + 1 + ', ';
-      }
-      const query = "INSERT INTO " + table + " (" + fields +") VALUES (" + values +")";
-      pool.connect((err, client, done) => {
-        if (err) throw err;
-        try {
-          csvData.forEach((row) => {
-            client.query(query, row, (err, res) => {
-              if (err) {
-                console.log(err.stack);
-              } else {
-                console.log("inserted " + res.rowCount + " row:", row);
-              }
-            });
-          });
-        } finally {
-          done();
+  try {
+    const { file, host, user, database, password, port, table, fields } =
+      req.body;
+    let stream = fs.createReadStream(file);
+    let csvData = [];
+    let csvStream = fastcsv
+      .parse()
+      .on("data", function (data) {
+        csvData.push(data);
+      })
+      .on("end", function () {
+        csvData.shift();
+        const pool = new Pool({ host, user, database, password, port });
+        let values = "";
+        const values_string = fields.split(",");
+        for (let i = 0; i < values_string.length; i++) {
+          values += "$" + (i + 1) + ", ";
         }
+        values = values.trim();
+        values = values.slice(0, values.length - 1);
+        const query =
+          "INSERT INTO " + table + " (" + fields + ") VALUES (" + values + ")";
+        pool.connect((err, client, done) => {
+          if (err) throw err;
+          try {
+            if(csvData.length > 12225) {
+              csvData.forEach((row) => {
+                client.query(query, row, (err, res) => {
+                  if (err) {
+                    console.log(row, err.stack);
+                  } else {
+                    console.log("Inserted");
+                  }
+                });
+              });
+            } else {
+              res.send("El archivo no debe de pasar las 12225 filas.");
+            }
+          } finally {
+            done();
+          }
+        });
       });
-    });
-  stream.pipe(csvStream);
-  res.send("Usuario alamacenado correctamente.");
+    stream.pipe(csvStream);
+    res.send("Verifique su consola.");
+  } catch (err) {
+    res.send("Error, intente de nuevo más tarde.");
+  }
 });
 
 app.listen(PORT, () => {
